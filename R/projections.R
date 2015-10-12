67a52195083dee11e61e4b96e06adf6126adcba5
Dewey Dunnington <dewey@fishandwhistle.net>
2015-10-12 14:28:20 -0300

    update to pass CMD check

--- R/projections.R
+++ R/projections.R
@@ -21,20 +21,3 @@
   newpoints <- sp::spTransform(spoints, sp::CRS(paste0("+init=epsg:", toepsg)))
   t(sp::coordinates(newpoints))
 }
-
-.geodist <- function(x1, y1, x2, y2, epsg) {
-  lonlat1 <- .tolatlon(x1, y1, epsg)
-  lonlat2 <- .tolatlon(x2, y2, epsg)
-
-  long1 <- .torad(lonlat1[1])
-  lat1 <- .torad(lonlat1[2])
-  long2 <- .torad(lonlat2[1])
-  lat2 <- .torad(lonlat2[2])
-  R <- 6371009 # Earth mean radius [m]
-  delta.long <- (long2 - long1)
-  delta.lat <- (lat2 - lat1)
-  a <- sin(delta.lat/2)^2 + cos(lat1) * cos(lat2) * sin(delta.long/2)^2
-  c <- 2 * asin(min(1,sqrt(a)))
-  d = R * c
-  return(d) # Distance in m
-}
